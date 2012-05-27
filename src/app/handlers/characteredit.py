33ed5fc29d490e7f30f7899f7c5222d4448c750c
mshrkato <mshrkato586@gmail.com>
2012-05-27 14:19:28 +0900

    made show function to havingItems by djangoFilter / 'checkHavingItem'

--- src/app/handlers/characteredit.py
+++ src/app/handlers/characteredit.py
@@ -11,6 +11,12 @@ class CharacterEdit(webapp.RequestHandler):
     def get(self):
         template_values = {
         	"sheet": {
+                "name": 'test',
+                "age": 25,
+                "sex": 'unknown',
+                "species": 'human',
+                "parasite": 'javelin',
+                
                 "powOriginal": 2,
                 "agiOriginal": 3,
                 "senOriginal": 4,
@@ -34,17 +40,35 @@ class CharacterEdit(webapp.RequestHandler):
                 "exerciseSkill": 5,
                 "hideSkill": 10,
                 "driveSkill": 15,
-                "controlSkill": 0
+                "controlSkill": 0,
+                
+				"intuitionSkill": 0,
+				"gambleSkill": 5,
+				"negotiateSkill": 10,
+				"sociabilitySkill": 10,
+				
+				"jentleSkill": 5,
+				"leadingSkill": 10,
+				"questionSkill": 15,
+				"charmSkill": 5,
+				
+				"dustlicence": 1,
+				"dustcoat": 0,
+				"dustSeal": 1,
+				"dustAwake": False
         	},
         	"demonic": {
         		"action": 10,
         		"energy": 20,
+        		"Lv": 1,
         		"powOffset": 0,
         		"agiOffset": 1,
         		"senOffset": 2,
         		"lucOffset": 3,
         		"intOffset": 4,
-        		"mntOffset": 5
+        		"mntOffset": 5,
+        		"highClasses":[{"name":"class1"}, {"name":"class2"}, {"name":"class3"}],
+        		"subClasses":[{"name":"class1"}, {"name":"class2"}, {"name":"class3"}]
         	}
         }
         path = join(dirname(dirname(dirname(__file__))), 'template', 'characteredit.html')
--- src/app/templatetags/parasiteweb.py
+++ src/app/templatetags/parasiteweb.py
@@ -9,6 +9,14 @@ def convertOri2Sta (value):
 # 元値と悪魔化補正から能力値を計算
 def calcDemonicSta (value, correct):
   return value/3 + correct
+  
+#アイテム所持
+def checkHavingItem (value):
+	if value == True:
+		return "checked='checked'"
+	else:
+		return ""
 
 register.filter(convertOri2Sta)
 register.filter(calcDemonicSta)
+register.filter(checkHavingItem)
