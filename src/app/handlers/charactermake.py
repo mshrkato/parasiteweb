21e64aa9c4109ffdbd04b18e2faf96afafd19e03
kinop <masataka.kinoshita@gmail.com>
2012-06-09 14:17:29 +0900

    change field names 3

--- src/app/handlers/charactermake.py
+++ src/app/handlers/charactermake.py
@@ -44,6 +44,19 @@ class CharacterMake(webapp.RequestHandler):
             "next": page+1,
             "back": page-1,
             "sheet": sheet,
+            "demonic": {
+                "action": 10,
+                "energy": 20,
+                "level": 1,
+                "powOffset": 0,
+                "agiOffset": 1,
+                "senOffset": 2,
+                "lucOffset": 3,
+                "intOffset": 4,
+                "mntOffset": 5,
+                "highClasses":[{"name":"class1"}, {"name":"class2"}, {"name":"class3"}],
+                "subClasses":[{"name":"class1"}, {"name":"class2"}, {"name":"class3"}]
+            }
         }
         path = join(dirname(dirname(dirname(__file__))), 'template', self.page_templates[page])
         self.response.out.write(template.render(path,template_values))
