047fe0639e361ebad22628d8ad3964f810fc10d0
mshrkato <mshrkato586@gmail.com>
2012-06-09 15:25:30 +0900

    modified class registration/ set battleOffset etc...

--- src/app/handlers/admin.py
+++ src/app/handlers/admin.py
@@ -12,28 +12,56 @@ class registClass(webapp.RequestHandler):
 
 
     def post(self):
-    	#登録or削除
-        parasite = parasiteData()
-        parasite.parasiteName = self.request.get('parasiteName')
-        parasite.parasiteClassName = self.request.get('parasiteClassName')
-        parasite.powOffset = int(self.request.get('powOffset'))
-        parasite.agiOffset = int(self.request.get('agiOffset'))
-        parasite.senOffset = int(self.request.get('senOffset'))
-        parasite.lucOffset = int(self.request.get('lucOffset'))
-        parasite.intOffset = int(self.request.get('intOffset'))
-        parasite.mntOffset = int(self.request.get('mntOffset'))
-        parasite.put()
+        if self.request.get('edit'):
+        	a = 1    
+        elif self.request.git('delete'):
+            b = 2
+        else:
+            #登録or削除
+            parasite = parasiteData()
+            
+            #No
+            parasite.parasiteNumber = int(self.request.get('parasiteNumber'))
+            parasite.parasiteName = self.request.get('parasiteName')
+            parasite.parasiteClassName = self.request.get('parasiteClassName')
+
+            #status
+            parasite.powOffset = int(self.request.get('powOffset'))
+            parasite.agiOffset = int(self.request.get('agiOffset'))
+            parasite.senOffset = int(self.request.get('senOffset'))
+            parasite.lucOffset = int(self.request.get('lucOffset'))
+            parasite.intOffset = int(self.request.get('intOffset'))
+            parasite.mntOffset = int(self.request.get('mntOffset'))
+            
+            #battle
+            parasite.punchAttack = int(self.request.get('punchAttack'))
+            parasite.punchDeffence = int(self.request.get('punchDeffence'))
+            parasite.shotAttack = int(self.request.get('shotAttack'))
+            parasite.shotDeffence = int(self.request.get('shotDeffence'))
+            parasite.specialAttack = int(self.request.get('specialAttack'))
+            parasite.specialDeffence = int(self.request.get('specialDeffence'))
+            
+            #other
+            parasite.actionOffset = int(self.request.get('actionOffset'))
+            parasite.energyOffset = int(self.request.get('energy'))
+            
+            #highClass
+            highClassA = int(self.request.get('highClassA'))
+            highClassB = int(self.request.get('highClassB'))
+            
+            parasite.put()
 
         self.show_page()
         
     def show_page(self):
-    	#登録済み寄生体を取得
-		q = parasiteData.all()
-		classes = q.fetch(10)
+        #登録済み寄生体を取得
+        q = parasiteData.all()
+        q.order("-parasiteNumber")
+        classes = q.fetch(200)
 
-		#渡して表示
-		template_values = {
-			"classes": classes,
-		}
-		path = join(dirname(dirname(dirname(__file__))), 'template', 'admin_registrationClass.html')
-		self.response.out.write(template.render(path,template_values))
+        #渡して表示
+        template_values = {
+            "classes": classes,
+        }
+        path = join(dirname(dirname(dirname(__file__))), 'template', 'admin_registrationClass.html')
+        self.response.out.write(template.render(path,template_values))
--- src/app/models/parasiteData.py
+++ src/app/models/parasiteData.py
@@ -2,6 +2,9 @@ from google.appengine.ext import db
 
 class parasiteData(db.Model):
 	#name
+	parasiteNumber = db.IntegerProperty()
+
+	#name
 	parasiteName = db.StringProperty()
 	#className
 	parasiteClassName = db.StringProperty()
@@ -15,10 +18,22 @@ class parasiteData(db.Model):
 	mntOffset = db.IntegerProperty()
 	
 	#battleOffset
+	punchAttack = db.IntegerProperty()
+	punchDeffence = db.IntegerProperty()
+	shotAttack = db.IntegerProperty()
+	shotDeffence = db.IntegerProperty()
+	specialAttack = db.IntegerProperty()
+	specialDeffence = db.IntegerProperty()
+	
+	#actionOffset
+	actionOffset = db.IntegerProperty()
+	
+	#energy
+	energyOffset = db.IntegerProperty()
 	
 	#highClass
-	highClasses_Upper = db.StringProperty()
-	highClasses_Down = db.StringProperty()
+	highClassA = db.StringProperty()
+	highClassB = db.StringProperty()
 	
 	def get_by_string(self, name):
 		return self.__dict__['_' + name]
